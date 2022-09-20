b073291d22dcb77c0f9a2952ceb0540071839031
Gábor Gyebnár <korteur@gmail.com>
2022-09-20 13:43:52 +0200

    Blender import basics (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod file;
+mod types;
+mod render;
+
+use anyhow::Result;
+use crate::file::blend_loader::load_blend_file;
+use crate::types::{Mesh, Object, Vertex};
+
+fn main() -> Result<()> {
+    let object = load_blend_file("app/file.blend")?;
+    println!("{:#?}", object);
+    Ok(())
 }
+
