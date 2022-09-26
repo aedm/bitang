1c06f03f0ac6dba3fd7081c1bbc63faa3dd60c96
aedm <korteur@gmail.com>
2022-09-26 14:05:44 +0200

    Vulkan renders the mesh

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,6 @@
+use ash::vk;
+use memoffset::offset_of;
+
 pub type Vertex = ([f32; 3], [f32; 3], [f32; 2]);
 pub type Face = [Vertex; 3];
 
@@ -13,4 +16,45 @@ pub struct Object {
     pub rotation: [f32; 3],
     pub scale: [f32; 3],
     pub mesh: Mesh,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct VertexV3 {
+    pub pos: [f32; 4],
+    pub color: [f32; 4],
+    pub tex_coord: [f32; 2],
+}
+
+impl VertexV3 {
+    pub fn get_binding_descriptions() -> [vk::VertexInputBindingDescription; 1] {
+        [vk::VertexInputBindingDescription {
+            binding: 0,
+            stride: std::mem::size_of::<Self>() as u32,
+            input_rate: vk::VertexInputRate::VERTEX,
+        }]
+    }
+
+    pub fn get_attribute_descriptions() -> [vk::VertexInputAttributeDescription; 3] {
+        [
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 0,
+                format: vk::Format::R32G32B32A32_SFLOAT,
+                offset: offset_of!(Self, pos) as u32,
+            },
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 1,
+                format: vk::Format::R32G32B32A32_SFLOAT,
+                offset: offset_of!(Self, color) as u32,
+            },
+            vk::VertexInputAttributeDescription {
+                binding: 0,
+                location: 2,
+                format: vk::Format::R32G32_SFLOAT,
+                offset: offset_of!(Self, tex_coord) as u32,
+            },
+        ]
+    }
 }
