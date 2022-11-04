064dc4628e754f18e51854e83baf7d53fe46efd3
aedm <korteur@gmail.com>
2022-11-04 17:32:00 +0100

    Vertex attributes

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -30,6 +30,7 @@ use cgmath::{Matrix3, Matrix4, Point3, Rad, Vector3};
 use egui::plot::{HLine, Line, Plot, Value, Values};
 use egui::{Color32, ColorImage, Ui};
 use egui_vulkano::UpdateTexturesResult;
+use glam::Vec3;
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, CpuBufferPool, TypedBufferAccess};
 use vulkano::command_buffer::{
     AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer, SubpassContents,
@@ -42,7 +43,9 @@ use vulkano::image::view::ImageView;
 use vulkano::image::{ImageAccess, ImageUsage, SwapchainImage};
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
-use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
+use vulkano::pipeline::graphics::vertex_input::{
+    BuffersDefinition, VertexMemberInfo, VertexMemberTy,
+};
 use vulkano::pipeline::graphics::viewport::Viewport;
 use vulkano::pipeline::graphics::viewport::ViewportState;
 use vulkano::pipeline::Pipeline;
@@ -58,11 +61,38 @@ use winit::window::{Fullscreen, Window, WindowBuilder};
 
 #[derive(Default, Debug, Clone, Copy, Pod, Zeroable)]
 #[repr(C)]
-pub struct Vertex {
+pub struct Vertex3 {
     position: [f32; 3],
+    normal: [f32; 3],
+    tangent: [f32; 3],
+    uv: [f32; 2],
+    padding: f32,
 }
 
-vulkano::impl_vertex!(Vertex, position);
+// unsafe impl vulkano::pipeline::graphics::vertex_input::Vertex for Vertex {
+//     fn member(name: &str) -> Option<VertexMemberInfo> {
+//         match name {
+//             "position" => Some(VertexMemberInfo {
+//                 offset: 0,
+//                 ty: VertexMemberTy::F32,
+//                 array_size: 3,
+//             }),
+//             // "position" => Some(VertexMemberInfo {
+//             //     offset: 0,
+//             //     ty: VertexMemberTy::F32,
+//             //     array_size: 3,
+//             // }),
+//             // "position" => Some(VertexMemberInfo {
+//             //     offset: 0,
+//             //     ty: VertexMemberTy::F32,
+//             //     array_size: 3,
+//             // }),
+//             _ => None,
+//         }
+//     }
+// }
+
+vulkano::impl_vertex!(Vertex3, position, normal, tangent, uv, padding);
 
 pub struct VulkanRenderer {
     pub device: Arc<Device>,
@@ -85,13 +115,19 @@ mod vs {
 				#version 450
 
 				layout(location = 0) in vec3 position;
+				layout(location = 1) in vec3 normal;
+				layout(location = 2) in vec3 tangent;
+				layout(location = 3) in vec2 uv;
                 layout(set = 0, binding = 0) uniform Data { vec4 e; vec2 viewport_adjust; } uniforms;
+
+				layout(location = 0) out vec2 v_uv;
 
 				void main() {
                     float sn = sin(uniforms.e.x);
                     float cs = cos(uniforms.e.x);
                     vec2 xy = position.xy;
 					gl_Position = vec4(vec2(xy.x *sn + xy.y * cs, -xy.x * cs + xy.y * sn) * uniforms.viewport_adjust, position.z, 1.0);
+                    v_uv = uv;
 				}
 			"
     }
@@ -103,10 +139,12 @@ mod fs {
         src: "
 				#version 450
 
+				layout(location = 0) in vec2 v_uv;
+
 				layout(location = 0) out vec4 f_color;
 
 				void main() {
-					f_color = vec4(1.0, 0.0, 0.0, 1.0);
+					f_color = vec4(v_uv, 0.0, 1.0);
 				}
 			"
     }
@@ -114,20 +152,20 @@ mod fs {
 
 pub struct Drawable {
     pub pipeline: Arc<GraphicsPipeline>,
-    pub vertex_buffer: Arc<CpuAccessibleBuffer<[Vertex]>>,
+    pub vertex_buffer: Arc<CpuAccessibleBuffer<[Vertex3]>>,
     pub uniform_buffer: CpuBufferPool<vs::ty::Data>,
 }
 
 pub struct DemoApp {
-    // vertex_buffer: Arc<CpuAccessibleBuffer<[Vertex]>>,
-    // pipeline: Arc<GraphicsPipeline>,
     start_time: Instant,
-    // uniform_buffer: CpuBufferPool<vs::ty::Data>,
     drawable: Option<Box<Drawable>>,
 }
 
 impl DemoApp {
     pub fn new(renderer: &VulkanRenderer) -> Self {
+        use vulkano::pipeline::graphics::vertex_input::Vertex;
+        let pos = Vertex3::member("position").unwrap();
+        println!("sdfgljh {} {:?} {}", pos.array_size, pos.ty, pos.offset);
         DemoApp {
             start_time: Instant::now(),
             drawable: None,
@@ -140,29 +178,21 @@ impl DemoApp {
             .faces
             .iter()
             .flatten()
-            .map(|v| Vertex {
+            .map(|v| Vertex3 {
                 position: [v.0[0], v.0[1], v.0[2]],
+                normal: [v.1[0], v.1[1], v.1[2]],
+                tangent: [0.0, 0.0, 0.0],
+                uv: [v.2[0], v.2[1]],
+                padding: 0.0,
             })
-            .collect::<Vec<Vertex>>();
+            .collect::<Vec<Vertex3>>();
 
         let vertex_buffer = {
             CpuAccessibleBuffer::from_iter(
                 renderer.device.clone(),
                 BufferUsage::all(),
                 false,
-                vertices, // [
-                          //     Vertex {
-                          //         position: [-0.5, -0.25],
-                          //     },
-                          //     Vertex {
-                          //         position: [0.0, 0.5],
-                          //     },
-                          //     Vertex {
-                          //         position: [0.25, -0.1],
-                          //     },
-                          // ]
-                          // .iter()
-                          // .cloned(),
+                vertices,
             )
             .unwrap()
         };
@@ -179,7 +209,7 @@ impl DemoApp {
         );
 
         let pipeline = GraphicsPipeline::start()
-            .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
+            .vertex_input_state(BuffersDefinition::new().vertex::<Vertex3>())
             .vertex_shader(vs.entry_point("main").unwrap(), ())
             .input_assembly_state(InputAssemblyState::new())
             .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
