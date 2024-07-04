ec43861c3ff87fcf1cb9b7037a98e73b7dca26ac
Gábor Gyebnár <korteur@gmail.com>
2024-07-04 10:40:57 +0200

    Tweak

--- demo/charts/sample-chart-1/backdrop.fs.glsl
+++ demo/charts/sample-chart-1/backdrop.fs.glsl
@@ -4,8 +4,7 @@ layout (location = 0) in vec3 v_ray_direction;
 
 layout (location = 0) out vec4 f_color;
 
-layout (set = 1, binding = 1) uniform sampler2D noise;
-layout (set = 1, binding = 2) uniform sampler2D env;
+layout (set = 1, binding = 1) uniform sampler2D env;
 
 layout (set = 1, binding = 0) uniform Uniforms {
     float g_app_time;
@@ -17,15 +16,16 @@ layout (set = 1, binding = 0) uniform Uniforms {
 };
 
 void main() {
-    float d = dot(normalize(v_ray_direction), g_light_dir);
-    d = (d + 1) * 0.5;
-    vec3 col = mix(col1, col2, d);
-    col = pow(col, args2.rgb);
-
-    f_color = vec4(col, 1);
     
     #if IMAGE_BOUND_TO_SAMPLER_ENV
         vec4 c = textureLod(env, v_ray_direction.zx, 7.5);
         f_color = vec4(c.rgb, 1);
+    #else 
+        float d = dot(normalize(v_ray_direction), g_light_dir);
+        d = (d + 1) * 0.5;
+        vec3 col = mix(col1, col2, d);
+        col = pow(col, args2.rgb);
+
+        f_color = vec4(col, 1);
     #endif
 }
